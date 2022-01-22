d387ca3621a13b2d4589113fda7405e3f5fa0a2a
SabinaJarabkova <jarabkova.sabina@gmail.com>
2022-01-22 10:32:45 +0100

    1 file modified, 1 added

--- ukol1.js
+++ ukol1.js
@@ -1,1 +1,18 @@
-console.log("Ahoj")
+console.log("Ahoj");
+
+alert("POZOR!");
+
+prompt("Kolik mas let?")
+
+let // takto zacina premenna
+var // vytvorenie premennej (ale zastarane)
+const // vytvorenie konstanty
+
+let jmeno = "sabina";
+let vek = 29;
+let povolani = "programator";
+
+console.log(jmeno);
+
+let  [nazev];
+let [nazev] = [hodnota];
