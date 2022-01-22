538462378d03dd405688b965d4ea250d7bcc4fec
SabinaJarabkova <jarabkova.sabina@gmail.com>
2022-01-22 11:33:59 +0100

    1 file modified

--- lekce.js
+++ lekce.js
@@ -3,7 +3,13 @@ console.log(odpoved);
 
 //pretypovanie retazca
 
-//pretypovanie an cislo Number('450')
+//pretypovanie na cislo Number('450')
 
 let odpovedAkoCislo = prompt('Kolko mas rokov?');
 console.log(Number(odpovedAkoCislo));
+
+// alebo druhy sposob, postupne po krokoch
+
+let odpoved = prompt('Kolko mas rokov?');
+let pretypovanie = Number(odpoved);
+console.log(pretypovanie);
