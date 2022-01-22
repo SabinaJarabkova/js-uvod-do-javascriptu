9602979b6a0b6858dd81507f5451633154c5b631
SabinaJarabkova <jarabkova.sabina@gmail.com>
2022-01-22 13:48:48 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import './js/ukol1.js';
-import './js/ukol2.js';
-import './js/lekce.js';
-import './js/podminky.js';
-import './js/ukol3.js';
+import '/js/ukol1.js';
+import '/js/ukol2.js';
+import '/js/lekce.js';
+import '/js/podminky.js';
+import '/js/ukol3.js';
