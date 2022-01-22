42093fed666c0027c4c99812ba4fafc1b71340cd
SabinaJarabkova <jarabkova.sabina@gmail.com>
2022-01-22 12:13:02 +0100

    1 file modified, 1 added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 import './ukol1.js';
 import './ukol2.js';
 import './lekce.js';
+import './podminky.js';
---
+++ podminky.js
@@ -0,0 +1,19 @@
+let vek = 25;
+
+if (VYRAZ) {
+  // vykona sa tato cast kodu pokial ten vyraz je pravda
+} else {
+  // tento kus kodu sa vykona pokial vyraz nie je pravda
+}
+
+if (vek < 18) {
+  console.log('Není plnoletý');
+} else {
+  console.log('Je plnoletý');
+
+  if (vek >= 21) {
+    console.log ('a moze soferovat kamion');
+  } else {
+    console.log ('a moze soferovat kamion');
+  }
+}
