c251e282be185d7fea7dd9ee8e254d56545605d3
SabinaJarabkova <jarabkova.sabina@gmail.com>
2022-01-22 14:08:38 +0100

    1 file modified

--- js/cyklus.js
+++ js/cyklus.js
@@ -27,3 +27,20 @@ i += 1;
 */
 
 // prikaz Break dany cyklus ukonci
+
+
+/* cyklus for
+
+for (na zaciatku; podmienka; na konci tela) {
+  to co sa ma vykonat
+}
+
+*/
+
+for (let y = 0; y < 6; y++) {
+  console.log(y);
+}
+
+// for cykus je dobre pouzit, ked uz v dany moment vieme pocet opakovani
+// while cyklus ked este nevieme kolkokrat sa to bdue opakovta alebo ak mame zlozitu podmienku
+// vsetko, co sa da napisat vo for cykle sa da napisat vo while cykle
